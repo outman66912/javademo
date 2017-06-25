f30077c2d426ce809a629ef5634b29349990371a
outman66912 <15951923234@163.com>
2017-06-25 17:06:40 +0800

    demo

---
+++ demo01/src/main/java/helloworld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/6/25.
+ */
+public class helloworld {
+    public static void main(String fa[]) {
+        hello h = new hello();
+        h.sayHello("scala");
+        System.out.println("dd");
+    }
+
+}
---
+++ demo01/src/main/scala/hello.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/6/25.
+ */
+class hello {
+  def sayHello(x: String): Unit = {
+    System.out.println("dd"+x);
+  }
+}
